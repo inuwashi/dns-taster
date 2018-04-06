c9ba38b92196cf5e6172bd431e5380cda2140c88
Adam Grandt <adam@inuwashi.net>
2018-04-05 23:43:55 -0400

    Version 0.1

--- DNSTaster.py
+++ DNSTaster.py
@@ -3,7 +3,7 @@ import click
 
 from dns import resolver, rdatatype
 from dns.exception import  DNSException
-from dns.resolver import NoAnswer, NoMetaqueries
+from dns.resolver import NoAnswer, NoMetaqueries, NoNameservers, Timeout
 from validators.domain import domain as valid_domain
 
 
@@ -14,20 +14,36 @@ class DNSTaster():
     def __init__(self, domain):
 
         self.resolver = resolver.Resolver()
+        self.resolver.timeout = 1
+        self.resolver.lifetime = 1
+
 
         #Validate the domain.
         if valid_domain(domain):
             self.domain = domain
         else:
-            raise ValueError('Could not validate domain')
+            raise DNSException('Could not validate domain')
 
         # Get name server
         self.name_servers = self._get_name_servers()
 
         if len(self.name_servers)<1:
-            raise ValueError('Could not get authoritative name servers.')
+            raise DNSException('Could not get authoritative name servers.')
+
+        # Get the DNS baseline
         self.baseline = self._get_baseline()
 
+
+    def taste(self,name_server):
+        """
+        Compare the available records for the domain from the provided
+        name server to the base line. Returns True or False and the record set.
+        """
+        dish = self._get_record_set(name_server)
+        if  dish == self.baseline:
+            return True, dish
+        else:
+            return False, dish
 
     def _get_name_servers(self, ns = '9.9.9.9'):
         """
@@ -50,15 +66,15 @@ class DNSTaster():
         for the provided domain from the provided name server 
         """
         self.resolver.nameservers=[socket.gethostbyname(name_server)]
-        records = []
+        records = set()
         for records_type in rdatatype._by_text.keys():
             try:
                 answesrs = self.resolver.query(self.domain, records_type)
-            except NoAnswer:
+            except (NoAnswer, NoMetaqueries, NoNameservers, Timeout) as e:
                 continue
 
             for rdata in answesrs:
-                records.append(rdata.to_text())
+                records.add(rdata)
         return records
 
     def _get_baseline(self):
@@ -66,7 +82,13 @@ class DNSTaster():
         Create a baseline records set
         """
         res = {}
+        base = False
         for name_server in self.name_servers:
-            res[name_server] = self._get_record_set(name_server)
-
-        print(res)
+            if base:
+                if set(self._get_record_set(name_server)) == base:
+                    continue
+                else:
+                    raise DNSException('Authoritative Name Servers do not agree.')
+            else:
+                base = self._get_record_set(name_server)
+        return base
--- dns_taster.py
+++ dns_taster.py
@@ -1,9 +1,9 @@
-import sys
+import sys, csv, pprint
 import click
 from dns import resolver, rdatatype
-from validators.domain import domain as valid_domain
+from dns.exception import  DNSException
 
-from DNSTaster import DNSTaster 
+from DNSTaster import DNSTaster
 
 
 
@@ -20,42 +20,71 @@ def taste(domain, verbose):
     Public DNS server lists was taken from : https://wiki.ipfire.org/dns/public-servers
     """
 
-    #click.clear()
-
-    # First make sure we have a domain
+    click.clear()
+    click.echo(' '+'#'*60)
+    click.echo(' #  Initiating DNS Taster for {}.'.format(domain))
     try:
         taster  = DNSTaster(domain)
-    except ValueError():
-        sys.exit(' ! Could not validate the domain.')
-
-    """
-
-    click.echo(' #  Thank you. Gonna taste some DNS records for {}'.format(domain))
-    #Now we get the name server, clean them up and save in a list.
-    name_servers = 
-    
-
-    #  If we didn't get any authretative name servers 
-    if len(name_servers)<1:
-        sys.exit(' E Could not get any primary name servers ')
+    except DNSException as e:
+        sys.exit(' ! Something went wrong: {}'.format(e))
 
 
-    click.echo(' #  Got authoritative name servers. Constructing base image.')
-    if verbose:
-        click.echo(' #  Name servers found:')
-        click.echo('\n'.join(['    * {}'.format(ns) for ns in name_servers]))
-
-    """
-
-
-
-
-
-
-
+    if verbose > 0:
+        click.echo(' #  Validated {}.'.format(taster.domain))
+        click.echo(' #  Found authoritative name servers.'.format(taster.domain))
+    if verbose > 1:
+        for ns in taster.name_servers:
+            click.echo(' #     {}'.format(ns))
+    if verbose > 0:
+        click.echo(' #  Generated DNS base line.')
+    if verbose > 1:
+        for record in taster.baseline:
+            print(' #    ',repr(record)[1:-1])
 
 
+    results = {
+        'Tasted Good':0,
+        'Tasted Bad':0,
+        'No Rules To Taste':0
+    }
 
+    click.echo(' '+'#'*60)
+    click.echo(' #  Gonna taste some DNS records for {}.'.format(domain))
+
+    with open('dns_server_list.csv', 'r') as ns_file:
+        ns_list = csv.reader(ns_file)
+        for ns in ns_list:
+            if verbose > 1: click.echo(' #  Tasting {} ({}).'.format(ns[0],ns[1]))
+            taste, funny_tasting_record_set = taster.taste(ns[1])
+
+            if len(funny_tasting_record_set)==0:
+                click.echo(' !    Name server {} did not return a rule set, skipping.'.format(ns[0]))
+                results["No Rules To Taste"] += 1
+                continue
+
+            if not taste:
+                click.echo(' !  Tasted something funny with {}.'.format(ns[0]))
+                results["Tasted Bad"] += 1
+
+                if verbose > 2:
+                    click.echo(' !  Funny tasting records:')
+                    for record in funny_tasting_record_set:
+                        print(' !    ',repr(record)[1:-1])
+
+                diff = taster.baseline.difference(funny_tasting_record_set)
+                click.echo(' !  Diff from baseline:')
+                for record in diff:
+                    print(' !    ',repr(record)[1:-1])
+            else:
+                if verbose > 0: click.echo(' #    {} tasted fine.'.format(ns[0]))
+                results["Tasted Good"] += 1
+
+
+    click.echo(' '+'#'*60)
+    click.echo(' # Taste test results:')
+    for r in results:
+        click.echo(' # {} Name server {}'.format(results[r],r))
+    click.echo(' '+'#'*60)
 
 
 if __name__ == '__main__':
